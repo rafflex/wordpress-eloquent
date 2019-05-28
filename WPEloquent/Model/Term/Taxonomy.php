3b3ccc4a96ee480d0ebe8ddc8cf58bd2af277cf1
Angus Allman <angus.allman@gmail.com>
2019-05-28 16:24:27 +0100

    enable similar functionality for taxonomy as post

--- WPEloquent/Model/Term/Taxonomy.php
+++ WPEloquent/Model/Term/Taxonomy.php
@@ -2,10 +2,27 @@
 
 namespace WPEloquent\Model\Term;
 
-class Taxonomy extends \Illuminate\Database\Eloquent\Model {
+class Taxonomy extends \Illuminate\Database\Eloquent\Model
+{
     protected $table = 'term_taxonomy';
+    protected $taxonomy = null;
 
-    public function term() {
+    public function newQuery()
+    {
+        $query = parent::newQuery();
+        if ($this->taxonomy) {
+            return $this->scopeType($query, $this->taxonomy);
+        }
+        return $query;
+    }
+
+    public function term()
+    {
         return $this->belongsTo(\WPEloquent\Model\Term::class, 'term_id', 'term_id');
+    }
+
+    public function scopeType($query, $type)
+    {
+        return $query->where('taxonomy', $type);
     }
 }
