4f3d17314d9161a28d72d745d7746f9c988bb30b
Angus Allman <angus.allman@gmail.com>
2019-06-21 10:25:16 +0100

    add an empty guarded array to the Post model

--- WPEloquent/Model/Post.php
+++ WPEloquent/Model/Post.php
@@ -4,69 +4,82 @@ namespace WPEloquent\Model;
 
 use \WPEloquent\Traits\HasMeta;
 
-class Post extends  \Illuminate\Database\Eloquent\Model {
+class Post extends \Illuminate\Database\Eloquent\Model
+{
 
     use HasMeta;
 
-    protected $table      = 'posts';
+    protected $table = 'posts';
     protected $primaryKey = 'ID';
     protected $post_type = null;
-    public $timestamps    = false;
+    public $timestamps = false;
+    protected $guarded = [];
 
     const CREATED_AT = 'post_date';
-	const UPDATED_AT = 'post_modified';
-	
-	public function newQuery() {
-		$query = parent::newQuery();
-		if($this->post_type) {
-			return $this->scopeType($query, $this->post_type);
-		}
-		return $query;
-	}
+    const UPDATED_AT = 'post_modified';
 
-    public function author () {
+    public function newQuery()
+    {
+        $query = parent::newQuery();
+        if ($this->post_type) {
+            return $this->scopeType($query, $this->post_type);
+        }
+        return $query;
+    }
+
+    public function author()
+    {
         return $this->hasOne(\WPEloquent\Model\User::class, 'ID', 'post_author');
     }
 
-    public function meta () {
+    public function meta()
+    {
         return $this->hasMany(\WPEloquent\Model\Post\Meta::class, 'post_id')
-                    ->select(['post_id', 'meta_key', 'meta_value']);
+            ->select(['post_id', 'meta_key', 'meta_value']);
     }
 
-    public function terms () {
+    public function terms()
+    {
         return $this->hasManyThrough(
-                    \WPEloquent\Model\Term\Taxonomy::class,
-                    \WPEloquent\Model\Term\Relationships::class,
-                    'object_id',
-                    'term_taxonomy_id'
-                )->with('term');
+            \WPEloquent\Model\Term\Taxonomy::class,
+            \WPEloquent\Model\Term\Relationships::class,
+            'object_id',
+            'term_taxonomy_id'
+        )->with('term');
     }
 
-    public function categories () {
+    public function categories()
+    {
         return $this->terms()->where('taxonomy', 'category');
     }
 
-    public function attachments () {
+    public function attachments()
+    {
         return $this->hasMany(\WPEloquent\Model\Attachment::class, 'post_parent', 'ID')->where('post_type', 'attachment');
     }
 
-    public function tags () {
+    public function tags()
+    {
         return $this->terms()->where('taxonomy', 'post_tag');
     }
 
-    public function comments () {
+    public function comments()
+    {
         return $this->hasMany(\WPEloquent\Model\Comment::class, 'comment_post_ID');
     }
 
-    public function scopeStatus ($query, $status = 'publish') {
+    public function scopeStatus($query, $status = 'publish')
+    {
         return $query->where('post_status', $status);
     }
 
-    public function scopeType ($query, $type = 'post') {
+    public function scopeType($query, $type = 'post')
+    {
         return $query->where('post_type', $type);
     }
 
-    public function scopePublished ($query) {
+    public function scopePublished($query)
+    {
         return $query->status('publish');
     }
 
