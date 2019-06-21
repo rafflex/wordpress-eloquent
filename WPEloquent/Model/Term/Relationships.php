b83dea113c617579c01d2157cbf6714d5c509a91
Angus Allman <angus.allman@gmail.com>
2019-06-21 11:47:32 +0100

    make term relationships creatable

--- WPEloquent/Model/Term/Relationships.php
+++ WPEloquent/Model/Term/Relationships.php
@@ -2,8 +2,11 @@
 
 namespace WPEloquent\Model\Term;
 
-class Relationships extends \Illuminate\Database\Eloquent\Model {
+class Relationships extends \Illuminate\Database\Eloquent\Model
+{
     protected $table = 'term_relationships';
     protected $primaryKey = 'term_taxonomy_id';
+    public $timestamps = false;
+    protected $guarded = [];
 
 }
